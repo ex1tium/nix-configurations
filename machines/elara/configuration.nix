8bd85107e04be624d558f4252f16164179ed0bd5
ex1tium <ex1tium@outlook.com>
2025-06-30 00:33:37 +0300

    fix: use qt5 version of sddm to ensure X11 compatibility in VM

--- machines/elara/configuration.nix
+++ machines/elara/configuration.nix
@@ -104,6 +104,7 @@
   # Force X11 for SDDM/KDE to avoid Wayland instability in VMs
   # Force SDDM to use X11 and software rendering in this VM
   services.displayManager.sddm = {
+    package = pkgs.sddm-qt5;
     wayland.enable = lib.mkForce false;
     settings = {
       General = {
